d9eb6e1d51ad1ba056a0b319b776c61360756326
Baybars ÖNAL <bay66bars@gmail.com>
2021-04-10 18:20:04 +0300

    some 2

--- Routers/Route.js
+++ Routers/Route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 var DescCont = require ('../Controls/Confg/DescCon')
 var KisiCont = require ('../Controls/Confg/KisiCont')
+var Ac = require('../Controls/Ac')
 
-
-router.get('/', Controls.PindexCon);
+router.get('/', Ac.PindexCon);
 //router.get('/deneme/Login', Controls.loginCon);
 //router.get('/ana', Controls.deneCon);
 
